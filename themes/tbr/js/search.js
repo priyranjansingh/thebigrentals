6912cd0d0c89d7bc7e44a04bfb83c2700df664c5
priyranjansingh <singh.priyranjan@gmail.com>
2015-06-21 00:05:24 +0530

    validation in search
    21-6-2015

--- themes/tbr/js/search.js
+++ themes/tbr/js/search.js
@@ -1,21 +1,101 @@
 var listPlaces = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  prefetch: base_url+'/properties/prefetch',
-  remote: {
-    url: base_url+'/properties/queries?list=%QUERY',
-    wildcard: '%QUERY'
-  }
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: base_url + '/properties/prefetch',
+    remote: {
+        url: base_url + '/properties/queries?list=%QUERY',
+        wildcard: '%QUERY'
+    }
 });
- 
+
 $('.typeahead').typeahead(null, {
-  name: 'where-to-go',
-  display: 'value',
-  source: listPlaces
+    name: 'where-to-go',
+    display: 'value',
+    source: listPlaces
 });
 
 $('#search-checkin').Zebra_DatePicker();
 
 $('#search-checkout').Zebra_DatePicker({
-  direction: 1    // boolean true would've made the date picker future only
+    direction: 1    // boolean true would've made the date picker future only
+});
+
+
+$('#search_form').submit(function(e) {
+    var search_location = $.trim($("#search-location").val()),
+            search_checkin = $.trim($("#search-checkin").val()),
+            search_checkout = $.trim($("#search-checkout").val()),
+            guest = $("#guest").val(),
+            error = {},
+            final_check = true;
+
+    if (search_location == '')
+    {
+        $("#search-location_em").show();
+        error['location_flag'] = false;
+    }
+    else
+    {
+        $("#search-location_em").hide();
+        error['location_flag'] = true;
+    }
+
+    if (search_checkin == '')
+    {
+        $("#search-checkin_em").show();
+        error['checkin_flag'] = false;
+    }
+    else
+    {
+        $("#search-checkin_em").hide();
+        error['checkin_flag'] = true;
+    }
+    if (search_checkout == '')
+    {
+        $("#search-checkout_em").show();
+        error['checkout_flag'] = false;
+    }
+    else
+    {
+        $("#search-checkout_em").hide();
+        error['checkout_flag'] = true;
+    }
+    if (guest == '')
+    {
+        $("#guest_em").show();
+        error['guest_flag'] = false;
+    }
+    else
+    {
+        $("#guest_em").hide();
+        error['guest_flag'] = true;
+    }
+    if (search_checkin !== '' && search_checkout !== '')
+    {
+        if (new Date(search_checkout) <= new Date(search_checkin))
+        {
+            $("#compare_em").show();
+            error['compare_flag'] = false;
+        }
+        else
+        {
+            $("#compare_em").hide();
+            error['compare_flag'] = true;
+        }
+    }
+
+    $.each(error, function(k, v) {
+        if (v === false) {
+            final_check = false;
+            //return false;
+        }
+    });
+    if (final_check) {
+        return true;
+    } else {
+        e.preventDefault();
+    }
+
+
+
 });
--- themes/tbr/views/layouts/banner.php
+++ themes/tbr/views/layouts/banner.php
@@ -47,25 +47,29 @@
   
     <div id="hero-search">
         <div id="search" class="width-limit">
-            <form accept-charset="UTF-8" action="<?php echo base_url(); ?>/properties/search" method="POST">
+            <form id="search_form" accept-charset="UTF-8" action="<?php echo base_url(); ?>/properties/search" method="POST">
                 <div class="col-sm-12 col-md-4">
                     <div>
                         <input name="query" id="search-location" class="form-control input-lg form-control-icon icon-location typeahead" type="text" placeholder="Where are you going?" value="" autocomplete="off">
                     </div>
                 </div>
+                <div class="errorMessage" id="search-location_em" style="display:none;">Destination cannot be blank.</div>  
                 <div class="col-xs-6 col-sm-3 col-md-2">
                   <div>
                     <input name="ch_in" id="search-checkin" class="form-control input-lg form-control-icon icon-calendar" type="text" placeholder="Check In" autocomplete="off">
                   </div>
                 </div>
+                <div class="errorMessage" id="search-checkin_em" style="display:none;">Check In time cannot be blank.</div>  
                 <div class="col-xs-6 col-sm-3 col-md-2">
                   <div>
                     <input name="ch_out" id="search-checkout" class="form-control input-lg form-control-icon icon-calendar" type="text" placeholder="Check Out" autocomplete="off">
                   </div>
                 </div>
+                 <div class="errorMessage" id="search-checkout_em" style="display:none;">Check Out time cannot be blank.</div>  
+                 <div class="errorMessage" id="compare_em" style="display:none;">Checkin time can not be greater than checkout time .</div>  
                 <div class="col-xs-6 col-sm-3 col-md-2">
                     <div class="form-group form-group-lg field_select">
-                        <select class="form-control select_styled base no-padding" name="guest">
+                        <select class="form-control select_styled base no-padding" id="guest" name="guest">
                             <option value="">Guests</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
@@ -80,6 +84,7 @@
                         </select>      
                     </div>
                 </div>
+                 <div class="errorMessage" id="guest_em" style="display:none;">Number of guest cannot be blank.</div>  
                 <div class="col-xs-6 col-sm-3 col-md-2">
                     <button id="search-button" class="btn btn-primary btn-lg btn-block tclick" data-tkey="Search" data-tloc="Search">Search</button>
                 </div>
