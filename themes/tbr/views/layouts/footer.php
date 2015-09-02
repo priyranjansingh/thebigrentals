e733b6e175aa9699b6158aa7f372852a7782a90a
priyranjansingh <singh.priyranjan@gmail.com>
2015-09-02 10:56:02 +0530

    sync

--- themes/tbr/views/layouts/footer.php
+++ themes/tbr/views/layouts/footer.php
@@ -1,136 +1,136 @@
-<?php $baseUrl = Yii::app()->theme->baseUrl; ?>
-<!-- FOOTER -->
-<footer class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3">
-                <div class="col">
-                    <h4>Contact us</h4>
-                    <h5>Europe</h5>
-                    <ul>
-                        <li>380 Wellington Street, Tower B, 6th Floor, London, Ontario, N6A 5B5</li>
-                        <li>Phone: +44 2071930407 </li>
-                        <li>Email: <a href="mailto:support@thebigrentals.com" title="Email Us">support@thebigrentals.com</a></li>
-                        <li>Skype: <a href="skype:thebigrentals?call" title="Skype us">thebigrentals</a></li>
-                    </ul>
-                </div>
-                <div class="col">
-                    <h5>Asia</h5>
-                    <ul>
-                        <li>E 14 - B, Sector 8, Noida, U.P. | PIN Code: 201301 | India</li>
-                        <li>Phone: +911204150334 | Fax: +911204150334 </li>
-                        <li>Email: <a href="mailto:support.india@thebigrentals.com" title="Email Us">support.india@thebigrentals.com</a></li>
-                        <li>Skype: <a href="skype:thebigrentals1?call" title="Skype us">thebigrentals1</a></li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="col-md-3">
-                <div class="col">
-                    <h4>Mailing list</h4>
-                    <p>Sign up if you would like to receive occasional treats from us.</p>
-                    <form class="form-horizontal form-light">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Your email address...">
-                            <span class="input-group-btn">
-                                <button class="btn btn-base" type="button">Go!</button>
-                            </span>
-                        </div>
-                    </form>
-                </div>
-            </div>
-
-            <div class="col-md-3">
-                <div class="col col-social-icons">
-                    <h4>Follow us on Facebook</h4>
-                    <iframe id="facebook_wid" src="http://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fthebigrentals&amp;width=220&amp;height=260&amp;colorscheme=&amp;show_faces=true&amp;stream=false&amp;header=false&amp;" style="border:none; overflow:hidden; width:220px; height:260px;background-color:white;"></iframe>
-                </div>
-            </div>
-
-            <div class="col-md-3">
-                <div class="col">
-                    <h4>About us</h4>
-                    <p class="no-margin">
-                        Lorem Ipsum is a dummy text.Lorem Ipsum is a dummy text.Lorem Ipsum is a dummy text.Lorem Ipsum is a dummy text.Lorem Ipsum is a dummy text.
-                    </p>
-                </div>
-            </div>
-        </div>
-
-        <hr>
-
-        <div class="row">
-            <div class="col-lg-9 copyright">
-                <?php echo date('Y'); ?> © The Big Rentals. All rights reserved.
-                <a href="#">Terms and conditions</a>
-            </div>
-            <div class="col-lg-3">
-                <a href="<?php echo base_url(); ?>" title="The Big Rentals" target="_blank" class="">
-                    <img src="<?php echo base_url(); ?>/images/logo.png" alt="The Big Rentals | Logo" class="pull-right">
-                </a>
-            </div>
-        </div>
-    </div>
-</footer>
-</div>
-<script>
-var base_url = "<?php echo base_url(); ?>";
-</script>
-<!-- Essentials -->
-<script src="<?php echo $baseUrl; ?>/js/modernizr.custom.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/bootstrap/js/bootstrap.min.js"></script>
-<script src="<?php echo $baseUrl; ?>/js/jquery.mousewheel-3.0.6.pack.js"></script>
-<script src="<?php echo $baseUrl; ?>/js/jquery.easing.js"></script>
-<script src="<?php echo $baseUrl; ?>/js/jquery.metadata.js"></script>
-<script src="<?php echo $baseUrl; ?>/js/jquery.hoverup.js"></script>
-<script src="<?php echo $baseUrl; ?>/js/jquery.hoverdir.js"></script>
-<script src="<?php echo $baseUrl; ?>/js/jquery.stellar.js"></script>
-<!--<script src="<?php echo $baseUrl; ?>/js/gmaps/google-maps-default.js"></script>-->
-
-<!-- Boomerang mobile nav - Optional  -->
-<script src="<?php echo $baseUrl; ?>/assets/responsive-mobile-nav/js/jquery.dlmenu.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/responsive-mobile-nav/js/jquery.dlmenu.autofill.js"></script>
-
-<!-- Forms -->
-<script src="<?php echo $baseUrl; ?>/assets/ui-kit/js/jquery.powerful-placeholder.min.js"></script> 
-<script src="<?php echo $baseUrl; ?>/assets/ui-kit/js/cusel.min.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/sky-forms/js/jquery.form.min.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/sky-forms/js/jquery.validate.min.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/sky-forms/js/jquery.maskedinput.min.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/sky-forms/js/jquery.modal.js"></script>
-
-<!-- Assets -->
-<script src="<?php echo $baseUrl; ?>/assets/hover-dropdown/bootstrap-hover-dropdown.min.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/page-scroller/jquery.ui.totop.min.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/mixitup/jquery.mixitup.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/mixitup/jquery.mixitup.init.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/fancybox/jquery.fancybox.pack8cbb.js?v=2.1.5"></script>
-<script src="<?php echo $baseUrl; ?>/assets/waypoints/waypoints.min.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/milestone-counter/jquery.countTo.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/easy-pie-chart/js/jquery.easypiechart.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/social-buttons/js/rrssb.min.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/nouislider/js/jquery.nouislider.min.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/owl-carousel/owl.carousel.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/bootstrap/js/tooltip.js"></script>
-<script src="<?php echo $baseUrl; ?>/assets/bootstrap/js/popover.js"></script>
-<?php if (Yii::app()->controller->action->id == "checkout"): ?>
-    <script src="<?php echo $baseUrl; ?>/js/tbr2c4sbc8pckg.js"></script>
-<?php endif; ?>
-<!-- Sripts for individual pages, depending on what plug-ins are used -->
-
-<!-- Boomerang App JS -->
-<script src="<?php echo $baseUrl; ?>/js/wp.app.js"></script>
-<script src="<?php echo $baseUrl; ?>/js/hcarousel.js"></script>
-<!--[if lt IE 9]>
-    <script src="<?php echo $baseUrl; ?>/js/html5shiv.js"></script>
-    <script src="<?php echo $baseUrl; ?>/js/respond.min.js"></script>
-<![endif]-->
-
-<!-- Temp -- You can remove this once you started to work on your project -->
-<!--<script src="<?php echo $baseUrl; ?>/js/jquery.cookie.js"></script>
-<script src="<?php echo $baseUrl; ?>/js/wp.switcher.js"></script>
-<script type="text/javascript" src="<?php echo $baseUrl; ?>/js/wp.ga.js"></script>-->
-<script src="<?php echo $baseUrl; ?>/js/typeahead.bundle.js"></script>
-<script src="<?php echo $baseUrl; ?>/js/zebra_datepicker.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places"></script>
+<?php $baseUrl = Yii::app()->theme->baseUrl; ?>
+<!-- FOOTER -->
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="col">
+                    <h4>Contact us</h4>
+                    <h5>Europe</h5>
+                    <ul>
+                        <li>380 Wellington Street, Tower B, 6th Floor, London, Ontario, N6A 5B5</li>
+                        <li>Phone: +44 2071930407 </li>
+                        <li>Email: <a href="mailto:support@thebigrentals.com" title="Email Us">support@thebigrentals.com</a></li>
+                        <li>Skype: <a href="skype:thebigrentals?call" title="Skype us">thebigrentals</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h5>Asia</h5>
+                    <ul>
+                        <li>E 14 - B, Sector 8, Noida, U.P. | PIN Code: 201301 | India</li>
+                        <li>Phone: +911204150334 | Fax: +911204150334 </li>
+                        <li>Email: <a href="mailto:support.india@thebigrentals.com" title="Email Us">support.india@thebigrentals.com</a></li>
+                        <li>Skype: <a href="skype:thebigrentals1?call" title="Skype us">thebigrentals1</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="col">
+                    <h4>Mailing list</h4>
+                    <p>Sign up if you would like to receive occasional treats from us.</p>
+                    <form class="form-horizontal form-light">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Your email address...">
+                            <span class="input-group-btn">
+                                <button class="btn btn-base" type="button">Go!</button>
+                            </span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="col col-social-icons">
+                    <h4>Follow us on Facebook</h4>
+                    <iframe id="facebook_wid" src="http://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fthebigrentals&amp;width=220&amp;height=260&amp;colorscheme=&amp;show_faces=true&amp;stream=false&amp;header=false&amp;" style="border:none; overflow:hidden; width:220px; height:260px;background-color:white;"></iframe>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="col">
+                    <h4>About us</h4>
+                    <p class="no-margin">
+                        Lorem Ipsum is a dummy text.Lorem Ipsum is a dummy text.Lorem Ipsum is a dummy text.Lorem Ipsum is a dummy text.Lorem Ipsum is a dummy text.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row">
+            <div class="col-lg-9 copyright">
+                <?php echo date('Y'); ?> © The Big Rentals. All rights reserved.
+                <a href="#">Terms and conditions</a>
+            </div>
+            <div class="col-lg-3">
+                <a href="<?php echo base_url(); ?>" title="The Big Rentals" target="_blank" class="">
+                    <img src="<?php echo base_url(); ?>/images/logo.png" alt="The Big Rentals | Logo" class="pull-right">
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+<script>
+var base_url = "<?php echo base_url(); ?>";
+</script>
+<!-- Essentials -->
+<script src="<?php echo $baseUrl; ?>/js/modernizr.custom.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php echo $baseUrl; ?>/js/jquery.mousewheel-3.0.6.pack.js"></script>
+<script src="<?php echo $baseUrl; ?>/js/jquery.easing.js"></script>
+<script src="<?php echo $baseUrl; ?>/js/jquery.metadata.js"></script>
+<script src="<?php echo $baseUrl; ?>/js/jquery.hoverup.js"></script>
+<script src="<?php echo $baseUrl; ?>/js/jquery.hoverdir.js"></script>
+<script src="<?php echo $baseUrl; ?>/js/jquery.stellar.js"></script>
+<!--<script src="<?php echo $baseUrl; ?>/js/gmaps/google-maps-default.js"></script>-->
+
+<!-- Boomerang mobile nav - Optional  -->
+<script src="<?php echo $baseUrl; ?>/assets/responsive-mobile-nav/js/jquery.dlmenu.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/responsive-mobile-nav/js/jquery.dlmenu.autofill.js"></script>
+
+<!-- Forms -->
+<script src="<?php echo $baseUrl; ?>/assets/ui-kit/js/jquery.powerful-placeholder.min.js"></script> 
+<script src="<?php echo $baseUrl; ?>/assets/ui-kit/js/cusel.min.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/sky-forms/js/jquery.form.min.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/sky-forms/js/jquery.validate.min.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/sky-forms/js/jquery.maskedinput.min.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/sky-forms/js/jquery.modal.js"></script>
+
+<!-- Assets -->
+<script src="<?php echo $baseUrl; ?>/assets/hover-dropdown/bootstrap-hover-dropdown.min.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/page-scroller/jquery.ui.totop.min.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/mixitup/jquery.mixitup.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/mixitup/jquery.mixitup.init.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/fancybox/jquery.fancybox.pack8cbb.js?v=2.1.5"></script>
+<script src="<?php echo $baseUrl; ?>/assets/waypoints/waypoints.min.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/milestone-counter/jquery.countTo.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/easy-pie-chart/js/jquery.easypiechart.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/social-buttons/js/rrssb.min.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/nouislider/js/jquery.nouislider.min.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/owl-carousel/owl.carousel.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/bootstrap/js/tooltip.js"></script>
+<script src="<?php echo $baseUrl; ?>/assets/bootstrap/js/popover.js"></script>
+<?php if (Yii::app()->controller->action->id == "checkout"): ?>
+    <script src="<?php echo $baseUrl; ?>/js/tbr2c4sbc8pckg.js"></script>
+<?php endif; ?>
+<!-- Sripts for individual pages, depending on what plug-ins are used -->
+
+<!-- Boomerang App JS -->
+<script src="<?php echo $baseUrl; ?>/js/wp.app.js"></script>
+<script src="<?php echo $baseUrl; ?>/js/hcarousel.js"></script>
+<!--[if lt IE 9]>
+    <script src="<?php echo $baseUrl; ?>/js/html5shiv.js"></script>
+    <script src="<?php echo $baseUrl; ?>/js/respond.min.js"></script>
+<![endif]-->
+
+<!-- Temp -- You can remove this once you started to work on your project -->
+<!--<script src="<?php echo $baseUrl; ?>/js/jquery.cookie.js"></script>
+<script src="<?php echo $baseUrl; ?>/js/wp.switcher.js"></script>
+<script type="text/javascript" src="<?php echo $baseUrl; ?>/js/wp.ga.js"></script>-->
+<script src="<?php echo $baseUrl; ?>/js/typeahead.bundle.js"></script>
+<script src="<?php echo $baseUrl; ?>/js/zebra_datepicker.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places"></script>
 <script src="<?php echo $baseUrl; ?>/js/search.js"></script>
--- themes/tbr/views/layouts/top_destination.php
+++ themes/tbr/views/layouts/top_destination.php
@@ -1,59 +1,59 @@
-<section class="slice bg-white bb">
-        <div class="wp-section">
-            <div class="container">
-                <div class="benefits hidden-phone">
-                <ul class="inline benefits-list text-center">
-                    <li>
-                        <h3>Over 1 million listings</h3>
-                        <div class="benefit-blurb visible-desktop">Bigrentals, Inc., the world leader in vacation rentals</div>
-                    </li>
-                    <li class="traveler-fees">
-                        <h3>No traveler fees</h3>
-                        <div class="benefit-blurb visible-desktop">Free to book with no hidden costs</div>
-                    </li>
-                    <li>
-                        <h3>Book with confidence</h3>
-                        <div class="benefit-blurb visible-desktop">One of Forbes' "Most Trustworthy Companies"</div>
-                    </li>
-                </ul>
-        </div>
-                
-                <div class="container-marketing js-containerBelowHero" style="margin-top: 120px;">
-                            <hr class="social-links-hr">
-                            <div class="social-links-container">
-                                <div class="social-links">
-	<iframe class="queLoad" src="http://www.facebook.com/plugins/like.php?href=https://www.facebook.com/thebigrentals&amp;layout=button_count&amp;show_faces=false&amp;width=92&amp;action=like&amp;colorscheme=light&amp;locale=en_US" rel="http://www.facebook.com/plugins/like.php?href=https://www.facebook.com/thebigrentals&amp;layout=button_count&amp;show_faces=false&amp;width=92&amp;action=like&amp;colorscheme=light&amp;locale=en_US" scrolling="no" frameborder="0" allowtransparency="true" hspace="0" id="facebook-iframe"></iframe>
-                                        <div id="___plusone_0" style="text-indent: 0px; margin: 0px; padding: 0px; border-style: none; float: none; line-height: normal; font-size: 1px; vertical-align: baseline; display: inline-block; width: 90px; height: 20px; background: transparent;"><iframe frameborder="0" hspace="0" marginheight="0" marginwidth="0" scrolling="no" style="position: static; top: 0px; width: 90px; margin: 0px; border-style: none; left: 0px; visibility: visible; height: 20px;" tabindex="0" vspace="0" width="100%" id="I0_1439294350598" name="I0_1439294350598" src="https://apis.google.com/se/0/_/+1/fastbutton?usegapi=1&amp;size=medium&amp;origin=http%3A%2F%2Fwww.thebigrentals.com&amp;url=https%3A%2F%2Fplus.google.com%2F%2Bhomeawaycom&amp;gsrc=3p&amp;ic=1&amp;jsh=m%3B%2F_%2Fscs%2Fapps-static%2F_%2Fjs%2Fk%3Doz.gapi.en_US.uMC9h9GYIK4.O%2Fm%3D__features__%2Fam%3DAQ%2Frt%3Dj%2Fd%3D1%2Ft%3Dzcms%2Frs%3DAGLTcCPwkFCgN_XKpTkRjvOUXMxZxl0DFA#_methods=onPlusOne%2C_ready%2C_close%2C_open%2C_resizeMe%2C_renderstart%2Concircled%2Cdrefresh%2Cerefresh%2Conload&amp;id=I0_1439294350598&amp;parent=http%3A%2F%2Fwww.bigrentals.com&amp;pfname=&amp;rpctoken=37562679" data-gapiattached="true" title="+1"></iframe></div>
-                                        <a href="http://www.pinterest.com/thebigrentals" class="pinterest-btn" target="_blank"><i class="icon-gt sprites-pinterest"></i>TheBigrentals</a>
-                                </div>
-                            </div>
-                </div>
-                
-                <div class="section-title-wr">
-                    <h3 class="section-title left"><span>Top Destination</span></h3>
-                </div>
-                <div id="destinations" class="row width-limit">
-                    <ul class="list-unstyled">
-                        <?php
-                           $tds = TopDestination::model()->findAll();
-                           $i = 1;
-                           foreach($tds as $td):
-                            if($i == 4 || $i == 5):
-                        ?>
-                            <li class="col-xs-6">
-                        <?php else: ?>
-                            <li class="col-xs-6 col-sm-4">
-                        <?php endif; ?>
-                                <a href="<?php echo $td->url; ?>" style="background-image: url('http://tbrs3.s3.amazonaws.com/<?php echo $td->image; ?>')">
-                                    <p><?php echo $td->place; ?></p>
-                                </a>
-                            </li>
-
-                        <?php $i++;
-                        endforeach;
-                        ?>
-                    </ul>
-                </div>
-            </div>
-        </div>
+<section class="slice bg-white bb">
+        <div class="wp-section">
+            <div class="container">
+                <div class="benefits hidden-phone">
+                <ul class="inline benefits-list text-center">
+                    <li>
+                        <h3>Over 1 million listings</h3>
+                        <div class="benefit-blurb visible-desktop">Bigrentals, Inc., the world leader in vacation rentals</div>
+                    </li>
+                    <li class="traveler-fees">
+                        <h3>No traveler fees</h3>
+                        <div class="benefit-blurb visible-desktop">Free to book with no hidden costs</div>
+                    </li>
+                    <li>
+                        <h3>Book with confidence</h3>
+                        <div class="benefit-blurb visible-desktop">One of Forbes' "Most Trustworthy Companies"</div>
+                    </li>
+                </ul>
+        </div>
+                
+                <div class="container-marketing js-containerBelowHero" style="margin-top: 120px;">
+                            <hr class="social-links-hr">
+                            <div class="social-links-container">
+                                <div class="social-links">
+	<iframe class="queLoad" src="http://www.facebook.com/plugins/like.php?href=https://www.facebook.com/thebigrentals&amp;layout=button_count&amp;show_faces=false&amp;width=92&amp;action=like&amp;colorscheme=light&amp;locale=en_US" rel="http://www.facebook.com/plugins/like.php?href=https://www.facebook.com/thebigrentals&amp;layout=button_count&amp;show_faces=false&amp;width=92&amp;action=like&amp;colorscheme=light&amp;locale=en_US" scrolling="no" frameborder="0" allowtransparency="true" hspace="0" id="facebook-iframe"></iframe>
+                                        <div id="___plusone_0" style="text-indent: 0px; margin: 0px; padding: 0px; border-style: none; float: none; line-height: normal; font-size: 1px; vertical-align: baseline; display: inline-block; width: 90px; height: 20px; background: transparent;"><iframe frameborder="0" hspace="0" marginheight="0" marginwidth="0" scrolling="no" style="position: static; top: 0px; width: 90px; margin: 0px; border-style: none; left: 0px; visibility: visible; height: 20px;" tabindex="0" vspace="0" width="100%" id="I0_1439294350598" name="I0_1439294350598" src="https://apis.google.com/se/0/_/+1/fastbutton?usegapi=1&amp;size=medium&amp;origin=http%3A%2F%2Fwww.thebigrentals.com&amp;url=https%3A%2F%2Fplus.google.com%2F%2Bhomeawaycom&amp;gsrc=3p&amp;ic=1&amp;jsh=m%3B%2F_%2Fscs%2Fapps-static%2F_%2Fjs%2Fk%3Doz.gapi.en_US.uMC9h9GYIK4.O%2Fm%3D__features__%2Fam%3DAQ%2Frt%3Dj%2Fd%3D1%2Ft%3Dzcms%2Frs%3DAGLTcCPwkFCgN_XKpTkRjvOUXMxZxl0DFA#_methods=onPlusOne%2C_ready%2C_close%2C_open%2C_resizeMe%2C_renderstart%2Concircled%2Cdrefresh%2Cerefresh%2Conload&amp;id=I0_1439294350598&amp;parent=http%3A%2F%2Fwww.bigrentals.com&amp;pfname=&amp;rpctoken=37562679" data-gapiattached="true" title="+1"></iframe></div>
+                                        <a href="http://www.pinterest.com/thebigrentals" class="pinterest-btn" target="_blank"><i class="icon-gt sprites-pinterest"></i>TheBigrentals</a>
+                                </div>
+                            </div>
+                </div>
+                
+                <div class="section-title-wr">
+                    <h3 class="section-title left"><span>Top Destination</span></h3>
+                </div>
+                <div id="destinations" class="row width-limit">
+                    <ul class="list-unstyled">
+                        <?php
+                           $tds = TopDestination::model()->findAll();
+                           $i = 1;
+                           foreach($tds as $td):
+                            if($i == 4 || $i == 5):
+                        ?>
+                            <li class="col-xs-6">
+                        <?php else: ?>
+                            <li class="col-xs-6 col-sm-4">
+                        <?php endif; ?>
+                                <a href="<?php echo $td->url; ?>" style="background-image: url('http://tbrs3.s3.amazonaws.com/<?php echo $td->image; ?>')">
+                                    <p><?php echo $td->place; ?></p>
+                                </a>
+                            </li>
+
+                        <?php $i++;
+                        endforeach;
+                        ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </section>
